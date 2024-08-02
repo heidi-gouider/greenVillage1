7fdff786f9f82c54257f7477e5072f799c75ade0
heidi-gouider <gouider.heidi@gmail.com>
2024-08-02 12:24:43 +0200

    front

---
+++ templates/partials/socialIcons.php
@@ -0,0 +1,10 @@
+ <!-- Logos réseaux sociaux avec une classe pour l'espacement -->
+ <li class="nav-item social-icon">
+                        <a href="#" class="social-icon"><i class="bi bi-facebook"></i></a>
+                    </li>
+                    <li class="nav-item social-icon">
+                        <a href="#" class="social-icon"><i class="bi bi-geo-alt"></i></a>
+                    </li>
+                    <li class="nav-item social-icon">
+                        <a href="#" class="social-icon"><i class="bi bi-instagram"></i></a>
+                    </li> 
